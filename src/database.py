df054ffcf2363bf3579151bdd29595fb35f4dd65
sadnastya@github.com <sadnastya@github.com>
2024-12-23 12:07:16 +0300

    first commit

---
+++ src/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+url = URL.create(
+    drivername="postgresql",
+    username=os.getenv("USERNAME_POSTGRESQL"),
+    password=os.getenv("PASSWORD_POSTGRESQL"),
+    host=os.getenv("HOST_POSTGRESQL"),
+    port=os.getenv("PORT_POSTGRESQL"),
+    database="app"
+)
+
+engine = create_engine(url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ src/dependencies.py
@@ -0,0 +1,11 @@
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from database import get_db
+from repositories import MoleculeRepository
+
+def get_molecule_repository(db: Session = Depends(get_db)) -> MoleculeRepository:
+    return MoleculeRepository(db)
+
+def get_test_molecule_repository(db: Session = get_db()) -> MoleculeRepository:
+    return MoleculeRepository(db)
